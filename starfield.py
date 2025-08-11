a5b44e5f2e3c19282fd93a7bdde7479cc5ede396
Daniele Campione <nonametekno@gmail.com>
2025-08-11 23:51:11 +0200

    v0.09a
    v0.09a

--- starfield.py
+++
@@ -1,7 +0,0 @@
-# Questo file è stato refactorizzato - le classi sono state spostate in file separati:
-# - Star -> star.py
-# - StarField -> star_field.py
-
-# Import per compatibilità
-from star import Star
-from star_field import StarField
